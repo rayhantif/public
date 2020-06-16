f0406756622ecade33f15c04132efe12a8c13a7d
seyl <rayhan.zharfan.tif18@polban.ac.id>
2020-06-16 08:17:49 +0100

    test

---
+++ precache-manifest.1f419f4a802373f4597e1a38d8592a09.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd54d0eaed8e9ea8ae3868f007042730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a0d507d312c299427f",
+    "url": "/static/css/2.d954d2ec.chunk.css"
+  },
+  {
+    "revision": "f430855632dd8c862912",
+    "url": "/static/css/main.5be3f488.chunk.css"
+  },
+  {
+    "revision": "c5a0d507d312c299427f",
+    "url": "/static/js/2.5c747718.chunk.js"
+  },
+  {
+    "revision": "f430855632dd8c862912",
+    "url": "/static/js/main.267089e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "388bb068a804db206922620f8399df8b",
+    "url": "/static/media/1585311447115_IMG20200310111154.388bb068.jpg"
+  },
+  {
+    "revision": "7361dc125119f343f8f20451104149e3",
+    "url": "/static/media/1585311453161_IMG20200310111318.7361dc12.jpg"
+  },
+  {
+    "revision": "bde00ba87cbec2e043a0396ecf5de3eb",
+    "url": "/static/media/1585311701014_IMG20200303110125.bde00ba8.jpg"
+  },
+  {
+    "revision": "82e8cc9614cdf0a6dc7cf1380430e7da",
+    "url": "/static/media/1585311751021_IMG20200229210300.82e8cc96.jpg"
+  },
+  {
+    "revision": "4664d8070f15b7c77f7144d5c3006b80",
+    "url": "/static/media/1585311800377_IMG20200310142129.4664d807.jpg"
+  },
+  {
+    "revision": "973a2617b96e1b1972e4eaf557ec9669",
+    "url": "/static/media/1585311807233_IMG20200302165028.973a2617.jpg"
+  },
+  {
+    "revision": "58d80b0f6b9ea98287d338d8110f844a",
+    "url": "/static/media/1585312204320_IMG20200225101010.58d80b0f.jpg"
+  },
+  {
+    "revision": "267448d7b57ad3aa4d6f3f41a7199901",
+    "url": "/static/media/1585312276769_IMG20200225100841.267448d7.jpg"
+  },
+  {
+    "revision": "43892103bf8816a9f307f6a24e9d49d5",
+    "url": "/static/media/1586747416003_IMG20200309115730.43892103.jpg"
+  },
+  {
+    "revision": "8db2923eeac118f9ae682306e198b303",
+    "url": "/static/media/1586748213629_IMG20200309121111.8db2923e.jpg"
+  },
+  {
+    "revision": "63a8a36f7b07619c114fe162e89a7941",
+    "url": "/static/media/1586749760771_IMG20200310130811.63a8a36f.jpg"
+  },
+  {
+    "revision": "ef4887f715eddb3451b5042e33b61931",
+    "url": "/static/media/1586750282828_IMG_20200224_103618.ef4887f7.jpg"
+  },
+  {
+    "revision": "2107e59769f155d122b424920c72dc2a",
+    "url": "/static/media/AsramaA.2107e597.png"
+  },
+  {
+    "revision": "ba8ab4480b441c7c5655dfb02709790e",
+    "url": "/static/media/AsramaB.ba8ab448.png"
+  },
+  {
+    "revision": "5ae17fc78224bd3979f6e67c9550e76b",
+    "url": "/static/media/AsramaC.5ae17fc7.png"
+  },
+  {
+    "revision": "1d1fdf1e622fa2a9b2cd3c855c338219",
+    "url": "/static/media/Direktorat.1d1fdf1e.png"
+  },
+  {
+    "revision": "7c35f98bcd5e4890bbc4d6c1bc29ec21",
+    "url": "/static/media/GedungA.7c35f98b.png"
+  },
+  {
+    "revision": "929e47a5c068a0fe1c480280978bc3b3",
+    "url": "/static/media/GedungB.929e47a5.png"
+  },
+  {
+    "revision": "1444acb1651edd5e0278c12b976a82dd",
+    "url": "/static/media/GedungC.1444acb1.png"
+  },
+  {
+    "revision": "c9ef20dae6def5a6fda8ca8f8eebecf9",
+    "url": "/static/media/GedungD.c9ef20da.png"
+  },
+  {
+    "revision": "5e1620cdc841860e130f2e29f003c567",
+    "url": "/static/media/GedungF.5e1620cd.png"
+  },
+  {
+    "revision": "7c0bc284a2f1a50ce3502549e07ada05",
+    "url": "/static/media/GedungH.7c0bc284.png"
+  },
+  {
+    "revision": "393217f13eff0edd0565ca522940bf2d",
+    "url": "/static/media/GedungSipil.393217f1.png"
+  },
+  {
+    "revision": "78e3197e86ee703576017aba049919ec",
+    "url": "/static/media/HanggarAero.78e3197e.png"
+  },
+  {
+    "revision": "db21e2fa084f163c9396e12ee91aa96f",
+    "url": "/static/media/JurusanTeknikKimia.db21e2fa.png"
+  },
+  {
+    "revision": "b44201e4e20312f4d21ac65338cca7e4",
+    "url": "/static/media/LH.b44201e4.png"
+  },
+  {
+    "revision": "04a3015beaa7a2876f81b55419292827",
+    "url": "/static/media/LabBis.04a3015b.png"
+  },
+  {
+    "revision": "1fb2e6f897bd131bdc0051f8ceba0a5a",
+    "url": "/static/media/LabTeknologiTeganganTinggi.1fb2e6f8.png"
+  },
+  {
+    "revision": "226b3d5c2dbdfbd22f5e14c1b950c0cb",
+    "url": "/static/media/Labmesinatas.226b3d5c.png"
+  },
+  {
+    "revision": "6290aacd8a45a255c592a82fc6e5cd94",
+    "url": "/static/media/Lapangbasket.6290aacd.png"
+  },
+  {
+    "revision": "011ecff82c3f99fe311fb242cfe72706",
+    "url": "/static/media/MKU.011ecff8.png"
+  },
+  {
+    "revision": "763de465eb730be45e63337b1e21bbf0",
+    "url": "/static/media/Mesindanelektro.763de465.png"
+  },
+  {
+    "revision": "7bb8d0c8e49298557fe6e1ed6011d17c",
+    "url": "/static/media/P2T.7bb8d0c8.png"
+  },
+  {
+    "revision": "1474f21785d2249d8770e88f2a8e04ec",
+    "url": "/static/media/Pendopo.1474f217.png"
+  },
+  {
+    "revision": "6fb817fdfe335e2448bea18d027f631a",
+    "url": "/static/media/Refrigrasi.6fb817fd.png"
+  },
+  {
+    "revision": "6e97714bd4572af13bb9e2e5145eeebf",
+    "url": "/static/media/SC.6e97714b.png"
+  },
+  {
+    "revision": "b4b460bc9720593c6c1a51bee60fe5aa",
+    "url": "/static/media/Teinggedungnaon.b4b460bc.png"
+  },
+  {
+    "revision": "af907f7f3568dd7701041a0d95266892",
+    "url": "/static/media/TekKimatas.af907f7f.png"
+  },
+  {
+    "revision": "159c88c98b0683d60078af392ee854a6",
+    "url": "/static/media/Welding.159c88c9.png"
+  },
+  {
+    "revision": "42321944001e061c0d91a75b6612ce2a",
+    "url": "/static/media/aji.42321944.png"
+  },
+  {
+    "revision": "d7f561add722ebe11cb23efa58df6ce7",
+    "url": "/static/media/akun.d7f561ad.png"
+  },
+  {
+    "revision": "c33d94a1430d8e43d0185d2a672f09dc",
+    "url": "/static/media/barang1.c33d94a1.png"
+  },
+  {
+    "revision": "0bcd4a2324f2229c8a240a087cc9c9b5",
+    "url": "/static/media/barang2.0bcd4a23.png"
+  },
+  {
+    "revision": "4e631f28bbdddc71d29b0fa007ee5920",
+    "url": "/static/media/barang3.4e631f28.png"
+  },
+  {
+    "revision": "32e2e1666f7fc64849f2f2580ba7d112",
+    "url": "/static/media/barang4.32e2e166.png"
+  },
+  {
+    "revision": "0f246b8414f0d8f6b24ea6203289e5bf",
+    "url": "/static/media/barang5.0f246b84.png"
+  },
+  {
+    "revision": "89b3955dc47811292b4f20bb1e4824bf",
+    "url": "/static/media/barang6.89b3955d.png"
+  },
+  {
+    "revision": "26f9d08fa2cede5ad170b3a4490656fd",
+    "url": "/static/media/febri.26f9d08f.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f5af2b5a4d996a905c30b645a24c47d8",
+    "url": "/static/media/ilham.f5af2b5a.png"
+  },
+  {
+    "revision": "a416abd96694e787f8cc6f56bfe9cda6",
+    "url": "/static/media/iman.a416abd9.png"
+  },
+  {
+    "revision": "99a1a46d52047eff95d1acf2883e6cb2",
+    "url": "/static/media/jimi.99a1a46d.png"
+  },
+  {
+    "revision": "96cdd7f7e95e48993411ee88a181dd06",
+    "url": "/static/media/rayhan.96cdd7f7.png"
+  },
+  {
+    "revision": "fa3561510dcca1bb6a8099ce042678e5",
+    "url": "/static/media/temuin1.fa356151.jpg"
+  },
+  {
+    "revision": "496af71eaacb062932c56095779dce7c",
+    "url": "/static/media/temuin2.496af71e.jpg"
+  },
+  {
+    "revision": "c953eaa93f4fa782ba287901361b2b24",
+    "url": "/static/media/temuin3.c953eaa9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71f2cbada03050a07fdd6949707b2fe4.js"
+  "/precache-manifest.1f419f4a802373f4597e1a38d8592a09.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
